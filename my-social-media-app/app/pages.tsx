8a3e9549f5b30790f441d1bedd00235609ac28f0
Tony <anthony.duncan117@gmail.com>
2025-01-06 19:43:12 -0500

    Initial commit

---
+++ my-social-media-app/app/pages.tsx
@@ -0,0 +1,9 @@
+import SignIn from "@/pages/SignIn";
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function Home() {
+  return (
+   <h3>hello</h3>
+  );
+}
---
+++ my-social-media-app/pages/Home.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Welcome to the homepage!</div>
+  )
+}
+
+export default Home
