3490c00d930b79b15e82a7006ab5b6b9fa5e9442
Thomas Martinez <devtommie114@gmail.com>
2025-01-06 20:22:42 -0500

    app working

--- my-social-media-app/app/pages.tsx
+++
@@ -1,9 +0,0 @@
-import SignIn from "@/pages/SignIn";
-import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-export default function Home() {
-  return (
-   <h3>hello</h3>
-  );
-}
